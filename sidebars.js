e9e6f9ead0df690e6a567cef18de78f080dab224
Toby Shao <107342456+spoonjunxi@users.noreply.github.com>
2022-07-15 16:42:40 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     初赛知识点:['computer','algorithm','common','skills'],
     算法与数据结构:['BIT','block','BST_and_more','dp','graph','mo_algorithm','segtree'],
-    数学专题:['number_diving','multiplicative_inverse'],
+    数学专题:['number_diving','multiplicative_inverse','gauss'],
     语言与STL:['functor'],
   },
 };
